356a1cc5bfa4664b98d92cf6a6ad5011f08f1d9f
SaintZ13 <calmdownkid13@gmail.com>
2017-08-25 15:17:55 -0400

    Revert "OmniSwitch: recalc cached colors on restart"
    This reverts commit 35a9edcabf032222d7e385a546212988ddbfdd22.

--- src/org/omnirom/omniswitch/ui/BitmapUtils.java
+++ src/org/omnirom/omniswitch/ui/BitmapUtils.java
@@ -278,11 +278,4 @@ public class BitmapUtils {
         image.draw(canvas);
         return new BitmapDrawable(resources, bmResult);
     }
-
-    public static void clearCachedColors() {
-        sTextPaint = null;
-        sLockedAppsPaint = null;
-        sDockedAppsPaint = null;
-        sDefaultBgPaint = null;
-    }
 }
